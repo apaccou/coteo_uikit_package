a1288316a53796a3c1860e4ca2ddc0a8465f8775
Alexandre PACCOU <a.paccou@coteo.com>
2015-05-08 18:33:10 +0200

    ajout d'uikit 2.20.3

--- controller.php
+++ controller.php
@@ -3,37 +3,48 @@ namespace Concrete\Package\CoteoUikitPackage;
 
 use Concrete\Core\Package\Package;
 use Concrete\Package\CoteoUikitPackage\Src\UikitGridFramework;
-use Concrete\Core;
+use Core;
+use Concrete\Core\Asset\AssetList;
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
 class Controller extends Package
 {
 
-  protected $pkgHandle = 'coteo_uikit_package';
-  protected $appVersionRequired = '5.7.3';
-  protected $pkgVersion = '0.0.1';
+    protected $pkgHandle = 'coteo_uikit_package';
+    protected $appVersionRequired = '5.7.3';
+    protected $pkgVersion = '0.0.1';
 
-  public function getPackageName()
-  {
-    return t("Concrete 5.7 Framework Uikit Package");
-  }
+    public function getPackageName()
+    {
+        return t("Concrete 5.7 Framework Uikit Package");
+    }
 
-  public function getPackageDescription()
-  {
-    return t("A package that installs the Uikit Framework for 5.7.3.");
-  }
+    public function getPackageDescription()
+    {
+        return t("A package that installs the 2.20.3 Uikit Framework for 5.7.3.");
+    }
 
-  public function install()
-  {
-    $pkg = parent::install();
-  }
+    public function install()
+    {
+        $pkg = parent::install();
+    }
 
-  // public function on_start()
-  // {
-  //   $manager = Core::make('manager/grid_framework');
-  //   $manager->extend('uikit', function($app) {
-  //     return new UikitGridFramework();
-  //   });
-  // }
+    public function on_start()
+    {
+        $manager = Core::make('manager/grid_framework');
+        $manager->extend('uikit', function($app) {
+            return new UikitGridFramework();
+        });
+
+        $al = AssetList::getInstance();
+        $al->register('javascript', 'uikit', 'uikit-2.20.3/js/uikit.min.js', array(), 'coteo_uikit_package');
+        $al->register('css', 'uikit', 'uikit-2.20.3/css/uikit.min.css', array(), 'coteo_uikit_package');
+
+        $al->registerGroup('uikit', array(
+            array('javascript', 'jquery'),
+            array('javascript', 'uikit'),
+            array('css', 'uikit'),
+        ));
+    }
 }
